70be179c082b1cd88b693d77525d1577a5aeffa2
samarveer <38134280+samarveer@users.noreply.github.com>
2021-09-02 13:49:49 -0400

    Initial commit

---
+++ src/GameTester.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameTester {
+	
+	Player pokemons = new Player();
+	
+	Pokemon p1 = new Pokemon("Pikachu", "Electric", 50, 50);
+}
---
+++ src/Player.java
@@ -0,0 +1,21 @@
+
+public class Player {
+
+	private String name;
+	private String type;
+	private int healthLeft;
+	private int healthTotal;
+
+	public Player(String name, String type, int healthLeft, int healthTotal) {
+		this.name = name;
+		this.type = type;
+		this.healthLeft = healthLeft;
+		this.healthTotal = healthTotal;
+		
+	}
+	
+	
+	
+	
+	
+}
